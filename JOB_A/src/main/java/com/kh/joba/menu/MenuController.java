462d1fc2b731ba579878d72a2f754ef300f8590d
JaePark <jaepark0909@gmail.com>
2020-12-22 22:38:27 +0900

    20201222_2238 commit:
    User Page Template Update

---
+++ JOB_A/src/main/java/com/kh/joba/menu/MenuController.java
@@ -0,0 +1,18 @@
+package com.kh.joba.menu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MenuController {
+	
+	@RequestMapping("/index.ma")
+	public String menu01() { return "index"; }
+	
+	@RequestMapping("/elements.ma")
+	public String menu02() { return "user/samplePage/elements"; }
+	
+	@RequestMapping("/generic.ma")
+	public String menu03() { return "user/samplePage/generic"; }
+
+}
