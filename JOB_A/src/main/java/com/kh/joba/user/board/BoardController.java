223dbb7b1457f979db8afd5110851b8975265169
JaePark <jaepark0909@gmail.com>
2020-12-23 14:33:44 +0900

    20201223_1431 commit:
    BoardController, notice, MenuController commit

---
+++ JOB_A/src/main/java/com/kh/joba/user/board/BoardController.java
@@ -0,0 +1,11 @@
+package com.kh.joba.user.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping("/notice.bo")
+	public String noticeList() { return "user/board/notice"; }
+}
