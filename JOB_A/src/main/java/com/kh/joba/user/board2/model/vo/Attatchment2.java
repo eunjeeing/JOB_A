2632137b5d342deda246cae5acc274bb29fde21a
정은지 <LG@DESKTOP-J40HQCG>
2020-12-27 23:44:20 +0900

    2020-12-27_23:43 commit

---
+++ JOB_A/src/main/java/com/kh/joba/user/board2/model/vo/Attatchment2.java
@@ -0,0 +1,18 @@
+package com.kh.joba.user.board2.model.vo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Attatchment2 {
+	
+	private int file_No;
+	private int board_No;
+	private String file_Origin;
+	private String file_Changed;
+	private String file_Path;
+
+}
