e7369e7847f73e39ee79dacf9e8d920c54134eb2
yoong <yoongs108@icloud.com>
2020-12-29 17:06:21 +0900

    20201229_1705 commit:
    calendarController_feature_yoon404

--- JOB_A/src/main/java/com/kh/joba/user/calendar/controller/CalendarController.java
+++ JOB_A/src/main/java/com/kh/joba/user/calendar/controller/CalendarController.java
@@ -1,10 +1,6 @@
 package com.kh.joba.user.calendar.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -14,6 +10,7 @@ public class CalendarController {
 	@RequestMapping("/calendar.do")
 	public String calendar() {
 		
+		System.out.println("calendar페이지 연결완료");
 		return "user/calendar/calendar";
 	}
 }
