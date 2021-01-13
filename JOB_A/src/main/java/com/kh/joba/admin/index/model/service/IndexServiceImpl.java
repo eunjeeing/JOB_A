1e6822e803f26afbaf411ea389dc7e80ab3a4e01
yoong <yoongs108@icloud.com>
2021-01-13 17:04:58 +0900

    20210113_1704 commit: carousel 진행중_feature_yoon404

--- JOB_A/src/main/java/com/kh/joba/admin/index/model/service/IndexServiceImpl.java
+++ JOB_A/src/main/java/com/kh/joba/admin/index/model/service/IndexServiceImpl.java
@@ -1,9 +1,13 @@
 package com.kh.joba.admin.index.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.joba.admin.index.model.dao.IndexDAO;
+import com.kh.joba.user.board2.blahblah.model.vo.Board2;
 
 
 @Service
@@ -13,8 +17,13 @@ public class IndexServiceImpl implements IndexService {
 
 	// 랭킹 서비스
 	@Override
-	public int selectRank() {
+	public List<Board2> selectRank() {
 		return indexDAO.selectAllCount();
+	}
+
+	@Override
+	public List<Map<String, String>> selectBoard() {
+		return indexDAO.carouselList();
 	}
 	
 	
